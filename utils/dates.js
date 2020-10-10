fe36d1d08ed701394b7841db1fec3bf3f6b58fce
Paul Craig <paul_craig_16@hotmail.com>
2020-10-10 01:08:08 -0400

    relative date logic a little more accurate

--- utils/dates.js
+++ utils/dates.js
@@ -6,27 +6,26 @@ import startOfDay from 'date-fns/startOfDay'
 import { space2Nbsp } from './strings.js'
 
 // 60 minutes * 24 hours = 1440
-export const getDateBeforeMidnightFromString = str =>
-  addMinutes(new Date(str), 1439)
+export const getDateBeforeMidnightFromString = (str) => addMinutes(new Date(str), 1439)
 
-export const displayDate = dateString =>
+export const displayDate = (dateString) =>
   space2Nbsp(format(getDateBeforeMidnightFromString(dateString), 'MMMM do'))
 
-export const relativeDate = dateString => {
+export const relativeDate = (dateString) => {
   const daysOffset = differenceInDays(
+    startOfDay(getDateBeforeMidnightFromString(dateString)),
     startOfDay(new Date()),
-    getDateBeforeMidnightFromString(dateString),
   )
 
   switch (daysOffset) {
     case 0:
       return 'That’s today! 🥳🥂🎊'
-    case -1:
+    case 1:
       return 'That’s tomorrow! 🤠'
     default:
       return `That’s in ${formatDistance(
-        getDateBeforeMidnightFromString(dateString),
-        new Date(),
+        startOfDay(getDateBeforeMidnightFromString(dateString)),
+        startOfDay(new Date()),
       )}.`
   }
 }
